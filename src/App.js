a6420100ddeb6297200ae1371b858ef6c1befa45
Aman Agrawal <amanagrawal2098@gmail.com>
2020-04-26 19:23:02 +0530

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React,{Component} from 'react';
 import Main from './components/MainComponent';
+import css from './App.css';
 class App extends Component {
   /*In this code the Menu component is called which is taking the dishes state inside it. now this state 
   is used as a prop here so it will be passed as a prop to the menu component in the menucompnent file.
   */ 
   render(){
     return (
-      <div>
+      <div className="css">
         <Main />
       </div>
     );
