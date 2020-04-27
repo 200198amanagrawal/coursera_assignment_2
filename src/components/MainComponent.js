76b6dd2c90d7dcd9c056997828a8e58136de0559
Aman Agrawal <amanagrawal2098@gmail.com>
2020-04-27 22:14:50 +0530

    modified header with routing links

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+
 /**
  * This code is basically taking the App.js code into it and then will just pass itself into the App.js to 
  * show the usage of Presentational and container component.
@@ -25,11 +23,21 @@ class Main extends Component {
  * the filter function is a stream which filters out the dish which has the same id.
  */
   render() {
+    const HomePage = () => {
+        return(
+            <Home 
+            />
+        );
+      }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        
+      <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+          </Switch>
         <Footer />
       </div>
     );
