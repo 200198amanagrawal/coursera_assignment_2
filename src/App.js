113119218a3a6214d9888f4f7239ec9498565f01
Aman Agrawal <amanagrawal2098@gmail.com>
2020-04-30 15:57:12 +0530

    completed the assignment 2 with aboutus

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render(){
     return (
       <BrowserRouter>
-      <div className="css">
+      <div>
         <Main />
       </div>
       </BrowserRouter>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 class Main extends Component {
@@ -45,12 +46,13 @@ class Main extends Component {
       />
         );
       }
-    return (
+    return (  
       <div>
         <Header />
         
       <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
